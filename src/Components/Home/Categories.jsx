174e725c7727f8bf014f7df85b0e8e18ff2bdb01
hadialhamza <hamzaglory@gmail.com>
2025-10-25 23:44:40 +0600

    category section redirect problem fixed

--- src/Components/Home/Categories.jsx
+++ src/Components/Home/Categories.jsx
@@ -14,49 +14,41 @@ import {
 const categories = [
   {
     name: "Building Blocks",
-    link: "/toys/building-blocks",
     icon: <FaCubes size={48} className="text-white" />,
     color: "bg-green-500",
   },
   {
     name: "Educational Toys",
-    link: "/toys/educational-toys",
     icon: <FaGraduationCap size={48} className="text-white" />,
     color: "bg-blue-500",
   },
   {
     name: "Stuffed Animals",
-    link: "/toys/stuffed-animals",
     icon: <FaHeart size={48} className="text-white" />,
     color: "bg-pink-500",
   },
   {
     name: "STEM Toys",
-    link: "/toys/stem-toys",
     icon: <FaFlask size={48} className="text-white" />,
     color: "bg-purple-500",
   },
   {
     name: "Arts & Crafts",
-    link: "/toys/arts-crafts",
     icon: <FaPaintBrush size={48} className="text-white" />,
     color: "bg-yellow-500",
   },
   {
     name: "Remote Control",
-    link: "/toys/remote-control",
     icon: <FaCar size={48} className="text-white" />,
     color: "bg-red-500",
   },
   {
     name: "Dollhouses",
-    link: "/toys/dollhouses",
     icon: <FaHome size={48} className="text-white" />,
     color: "bg-rose-500",
   },
   {
     name: "Puzzles",
-    link: "/toys/puzzles",
     icon: <FaPuzzlePiece size={48} className="text-white" />,
     color: "bg-cyan-500",
   },
@@ -76,8 +68,7 @@ const Categories = () => {
         </div>
         <div className="grid grid-cols-2 md:grid-cols-4 gap-6 md:gap-8">
           {categories.map((category) => (
-            <Link
-              to={category.link}
+            <div
               key={category.name}
               className="flex flex-col items-center p-6 bg-white rounded-xl shadow-md
                          hover:shadow-lg hover:-translate-y-1 transition-all duration-300"
@@ -90,7 +81,7 @@ const Categories = () => {
               <h3 className="text-xl font-semibold font-nunito mt-4 text-gray-800">
                 {category.name}
               </h3>
-            </Link>
+            </div>
           ))}
         </div>
       </div>
